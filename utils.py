08937550a7bf66fec5b10f4d30452ea5b213d10c
cvoluj <berezanskiydaniiil@gmail.com>
2024-11-24 20:12:10 +0200

    fix formatting

--- utils.py
+++ utils.py
@@ -3,15 +3,15 @@
 def format_order_data(order_data):
     order = order_data['order']
     details = {
-        "Номер замовлення": f"<b>{order.get('order_id', 'Немає даних')}</b>",
-        "Сума замовлення": f"<b>{order.get('total', 'Немає даних')}</b>",
-        "Статус замовлення": f"<b>{order.get('order_status', 'Немає даних')}</b>",
-        "Дата замовлення": f"<b>{order.get('date_added', 'Немає даних')}</b>",
-        "Спосіб доставки": f"<b>{order.get('shipping_method', 'Немає даних')}</b>",
-        "Місто": f"<b>{order.get('payment_city', 'Немає даних')}</b>",
-        "Адреса": f"<b>{order.get('payment_address_1', 'Немає даних')}</b>",
+        "Номер замовлення": f"*{order.get('order_id', 'Немає даних')}*",
+        "Сума замовлення": f"*{order.get('total', 'Немає даних')}*",
+        "Статус замовлення": f"*{order.get('order_status', 'Немає даних')}*",
+        "Дата замовлення": f"*{order.get('date_added', 'Немає даних')}*",
+        "Спосіб доставки": f"*{order.get('shipping_method', 'Немає даних')}*",
+        "Місто": f"*{order.get('payment_city', 'Немає даних')}*",
+        "Адреса": f"*{order.get('payment_address_1', 'Немає даних')}*",
         "Товари у замовленні": [
-            f"<b>{product.get('product_info', {}).get('name', 'Немає даних')}</b>"
+            f"*{product.get('product_info', {}).get('name', 'Немає даних')}*"
             for product in order.get('order_products', [])
         ]
     }
