ff4cfeff1037e714e0979c202533523c96751a00
cvoluj <berezanskiydaniiil@gmail.com>
2024-11-24 21:38:47 +0200

    test

--- messages.py
+++ messages.py
@@ -235,7 +235,7 @@ def show_order(viber_request, viber, orders_data, index):
     prev_page = {
         "Columns": 3,
         "Rows": 1,
-        "Text": format_text_with_color('\003C'),
+        "Text": format_text_with_color('\u003C'),
         "ActionBody": f"< {index - 1}",
         **buttons_settings,
     }
