f5eff60449bc60306a0e74f2d40809b39dc568f1
cvoluj <berezanskiydaniiil@gmail.com>
2025-01-10 20:55:59 +0200

    add login on each request

--- api/statistic_api.py
+++ api/statistic_api.py
@@ -2,7 +2,7 @@ import requests
 
 
 from settings import settings
-
+from api.startup_login import startup_login
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:35.0) Gecko/20100101 Firefox/35.0'
@@ -10,6 +10,7 @@ headers = {
 
 
 def synchronize_user(user_id, phone):
+    startup_login()
     api_url = f"https://cosmy.com.ua/index.php?route=api/v2/customer/createOrUpdateViber&api_token={settings.api_token}"
     data = {
             'telephone': phone,
@@ -23,6 +24,7 @@ def synchronize_user(user_id, phone):
 
 
 def delete_user_from_messenger(phone):
+    startup_login()
     messenger="viber"
     api_url = f"https://cosmy.com.ua/index.php?route=api/v2/customer/deleteMessenger&api_token={settings.api_token}"
     data = {
